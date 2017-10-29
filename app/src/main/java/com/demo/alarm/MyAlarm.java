49e052e2d3d302c6a873da1ad3b732746cb392c3
Siddharth Omar <siddomar@gmail.com>
2017-10-29 22:05:46 +0530

    Initial commit with screen wake-up, vibrator and ringtone

---
+++ app/src/main/java/com/demo/alarm/MyAlarm.java
@@ -0,0 +1,46 @@
+package com.demo.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.PowerManager;
+import android.util.Log;
+import android.os.Vibrator;
+
+/**
+ * Created by Siddharth Omar on 28-Oct-17.
+ */
+public class MyAlarm extends BroadcastReceiver {
+
+    //the method will be fired when the alarm is triggerred
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //you can check the log that it is fired
+        //Here we are actually not doing anything
+        //but you can do any task here that you want to be done at a specific time everyday
+        Log.d("MyAlarm", "Alarm just fired");
+        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = pm.isScreenOn();
+        if(isScreenOn==false)
+        {
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE,"MyLock");
+            wl.acquire(10000);
+            PowerManager.WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyCpuLock");
+
+            wl_cpu.acquire(10000);
+        }
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+
+}
